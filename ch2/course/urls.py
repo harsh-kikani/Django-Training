0875e672fb10b356674d2033bd6b04d9a313d9dc
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-08-29 15:47:46 +0530

    add variable in templates

--- ch2/course/urls.py
+++ ch2/course/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from course.views import learn_django, learn_fastapi
+from course.views import learn_django
+
 urlpatterns = [
     path('dj/', learn_django),
-    path('fst/',learn_fastapi),
+    #path('fst/',learn_fastapi),
 ]
--- ch2/course/views.py
+++ ch2/course/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def learn_django(req):
+#def learn_django(req):
     
     #return render(req, template_name, context=dic_name,
     #content_type=MIME_TYPE, status=None, using=None)
     
-    return render(req, 'course/django.html')
+ #   return render(req, 'course/django.html')
 
-def learn_fastapi(req):
-    return render(req, 'course/fastapi.html')
+#def learn_fastapi(req):
+#    return render(req, 'course/fastapi.html')
+
+# Example 1.1 - Variable
+
+def learn_django(req):
+    return render(req, 'course/django.html', context={'name': 'Django'})
+
+
+# Example 1.2 - Variable
+def learn_django(req):
+    return render(req, 'course/django.html', context={'name': 'Django'})
