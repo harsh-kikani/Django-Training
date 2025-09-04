8623c70062cc660ca122c7a287905b585481c3f7
harsh-kikani <harsh.kikani@exhibytesolution.com>
2025-09-04 18:02:59 +0530

    form field type argument and POST Request

--- ch4/student/forms.py
+++ ch4/student/forms.py
@@ -1,13 +1,80 @@
 from django import forms
+from django.core.validators import MinLengthValidator
 
+
+# class Registration(forms.Form):
+#     first_name = forms.CharField()
+#     last_name = forms.CharField()
+#     email = forms.EmailField()
+#     city = forms.CharField()
 
 class Registration(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    Name = forms.CharField()
     email = forms.EmailField()
-    city = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
     
 class Login(forms.Form):
     email = forms.EmailField()
     password = forms.CharField()
+    
+    
+#--------------Field Type example---------------
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),]
+
+INTERESTS_CHOICES = [
+    ('tech', 'Technology'),
+    ('art', 'Art'),
+    ('sports', 'Sports'),]
+
+    
+class DemoForm(forms.Form):
+    # Basic fields
+    name = forms.CharField()
+    email = forms.EmailField()
+    pin_code = forms.IntegerField()
+    
+    #Additional Field Types
+    age = forms.FloatField()
+    date_of_birth = forms.DateField()
+    appointment_time = forms.TimeField()
+    appointment_datetime = forms.DateTimeField()
+    is_subscribed = forms.BooleanField()
+    agree_terms = forms.NullBooleanField()
+    
+    # Choice Fields
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
+    
+    interests = forms.MultipleChoiceField(choices=INTERESTS_CHOICES)
+    
+    #File and url fields
+    profile_image = forms.ImageField()
+    resume = forms.FileField()
+    website = forms.URLField()
+    
+    #other specialized fields
+    phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$')
+    password = forms.CharField(widget=forms.PasswordInput())
+    slug = forms.SlugField()
+    ip_address = forms.GenericIPAddressField()
+    rating = forms.DecimalField()
+    
+    
+    
+# class DemoForm(forms.Form):
+#     name = forms.CharField(
+#         label="Full Name",
+#         max_length=100,
+#         label_suffix=":",
+#         initial="Enter your full name",
+#         help_text="Enter your legal name here",
+#         validators=[MinLengthValidator(3)]
+#     )
+    
+
+    
+    
     
--- ch4/student/views.py
+++ ch4/student/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from student.forms import Registration, Login
+from student.forms import Registration, Login, DemoForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 
-def registration(req):
-    fm = Registration()
+def registration(request):
+    if request.method == 'POST':
+        form = Registration(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['Name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            print('Name:', name)
+            print('Email:', email)
+            print('Password:', password)
+            return HttpResponseRedirect('/student/success/')
+            
+    else:    
+        form = Registration()
     # fm = Registration(field_order=['email', 'city'])
-    return render(req, 'student/registration.html', {'form': fm})
+    return render(request, 'student/registration.html', {'form': form})
+
+def reg_success(request):
+    return render(request, 'student/success.html')
 
 def login(req):
     fm = Login()
@@ -28,3 +44,12 @@ def login(req):
     #     'email': 'mehul@example.com','password': '1234'})
     
     return render(req, 'student/login.html', {'form': fm})
+
+
+
+#------------------DEMOFORM--------------------
+
+def demo_form(request):
+    form = DemoForm()
+    return render(request, 'student/demoform.html', {'form': form})
+
